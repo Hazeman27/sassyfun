cf397bb8751a4bb2f018fba07557914107b401ee
Hazeman <tremul27@gmail.com>
2019-01-12 10:10:50 +0100

    Masonry ordering done

--- js/masonry.js
+++ js/masonry.js
@@ -1,76 +1,82 @@
 
+class Masonry {
+
+    constructor(grid) {
+
+        this.grid = grid;
+        this.items = Array.from(grid.children);
+        this.columnCount = parseInt(window.getComputedStyle(this.grid).columnCount);
+    }
+
+    orderItemsLeftToRight() {
+
+        this.items.forEach(item => { item.remove(); });
+
+        for (let i = 0; i < this.columnCount; i++) {
+            
+            this.grid.appendChild(this.items[i]);
+
+            for (let j = 0; this.items[(this.columnCount * (j + 1)) + i] != undefined; j++)
+                this.grid.appendChild(this.items[(this.columnCount * (j + 1)) + i]);
+        }
+    }
+
+    fillWithRandomImages(imageContainer) {
+
+        const images = Array.from(imageContainer);
+        
+        images.forEach((image, index) => {
+            $(image).attr('src', 'https://source.unsplash.com/random/60' + index);
+        });
+    }
+}
+
 $(document).ready(() => {
 
-    // ** Filling with random images **
-    
-    const images = Array.from($('.masonry-grid__item__background'));
-    images.forEach((image, index) => {
-        
-        $(image).attr('src', 'https://source.unsplash.com/random/60' + index);
+    // ** ORDERING GRID ITEMS **
+
+    const grid = $('.masonry-grid')[0];
+    const imageContainer = $('.masonry-grid__item__background');
+
+    const masonry = new Masonry(grid);
+
+    masonry.fillWithRandomImages(imageContainer);
+    masonry.orderItemsLeftToRight();
+
+    $(window).resize(() => {
+        masonry.columnCount = parseInt(window.getComputedStyle(grid).columnCount);
+        masonry.orderItems();
     });
 
-    // ===============================
-    // New Line
-    /*const resizeGridItem = (item) => {
 
-        const grid = $('.masonry-grid')[0];
-        const rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
-        const rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
-        const imageHeight = item.querySelector('.masonry-grid__item__background').getBoundingClientRect().height;
-        const rowSpan = Math.ceil((imageHeight + rowGap) / (rowHeight + rowGap));
-
-        item.style.gridRowEnd = "span " + rowSpan;
-    }
-
-    const resizeAllGridItems = () => {
-        
-        const allItems = document.getElementsByClassName('masonry-grid__item');
-        
-        for(let i = 0; i < allItems.length; i++)
-            resizeGridItem(allItems[i]);
-    }
-
-    const resizeInstance = (instance) => {
-        
-        const item = instance.elements[0];
-        resizeGridItem(item);
-    }
-
-    window.onload = resizeAllGridItems();
-    window.addEventListener('resize', resizeAllGridItems);
-
-    const allItems = document.getElementsByClassName('masonry-grid__item');
-    
-    for(i = 0; i < allItems.length; i++)
-        if (allItems[i].getElementsByTagName('img').length > 0)
-            imagesLoaded(allItems[i], resizeInstance);
-*/
 
     // ** RATING **
+
+
 
     const ratingButton = $('.masonry-grid__item__rating');
     
     var rated = false;
-
+ 
     $(ratingButton).click(function() {
-
+ 
         const path = $($(this)).css('background-image');
         const rating = $(this);
         const currentRating = parseInt($(rating).text());
-
+ 
         if (rated) {
-            
+             
             $(rating).css('background-image', path.replace('heart-pressed.png', 'heart.png'));
             $(rating).text(currentRating - 1);
-            
+             
             rated = false;
         }
         else {
-            
+             
             $(rating).css('background-image', path.replace('heart.png', 'heart-pressed.png'));
             $(rating).text(currentRating + 1);
-
+ 
             rated = true;
         }
-    });
+    }); 
 });
